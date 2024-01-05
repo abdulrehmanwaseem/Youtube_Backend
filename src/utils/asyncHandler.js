d129019075cdbf7956fa5465bb164de02fb0977b
Abdul_Rehman / <151991988+abdul-rehman22@users.noreply.github.com>
2024-01-05 18:39:01 +0500

    Update asyncHandler.js

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -14,7 +14,7 @@ export { asyncHandler };
 //     try {
 //         await fn(req, res, next)
 //     } catch (error) {
-//         res.status(err.code || 500).json({
+//         res.status(error.code || 500).json({
 //             success: false,
 //             message: err.message
 //         })
